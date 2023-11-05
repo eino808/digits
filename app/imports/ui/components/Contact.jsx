328a6c553e7de51a8a7265e7fc714b1583c664e6
Elijah Tran Inofinada <elijahboy808@yahoo.com>
2023-11-05 00:58:19 -1000

    notes-2 finished in 37 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Image, ListGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Note from './Note';
+import AddNote from './AddNote';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 // eslint-disable-next-line react/prop-types
-const Contact = ({ contact }) => (
+const Contact = ({ contact, notes }) => (
   <Card className="h-100">
     {/* eslint-disable-next-line react/jsx-pascal-case */}
     <Card.Header>
@@ -20,6 +22,12 @@ const Contact = ({ contact }) => (
       {/* eslint-disable-next-line react/prop-types */}
       <Card.Text>{contact.description}</Card.Text>
     </Card.Body>
+    <ListGroup variant="flush">
+      {/* eslint-disable-next-line react/prop-types */}
+      {notes.map((note) => <Note key={note._id} note={note} />)}
+    </ListGroup>
+    {/* eslint-disable-next-line react/prop-types */}
+    <AddNote owner={contact.owner} contactId={contact._id} />
     {/* eslint-disable-next-line react/prop-types */}
     <Link to={`/edit/${contact._id}`}>Edit</Link>
   </Card>
@@ -33,8 +41,16 @@ Contact.propTypes = {
     address: PropTypes.string,
     image: PropTypes.string,
     description: PropTypes.string,
-    // _id: PropTypes.string,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
+PropTypes.arrayOf(PropTypes.shape({
+  note: PropTypes.string,
+  contactId: PropTypes.string,
+  owner: PropTypes.string,
+  createdAt: PropTypes.string,
+  _id: PropTypes.string,
+}));
 
 export default Contact;
